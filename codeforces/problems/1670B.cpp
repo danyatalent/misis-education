3a6e175e538c3ecb41f326d56069669f7c2708d5
Danya Kanevskiy <113839256+danyatalent@users.noreply.github.com>
2022-10-01 14:00:40 +0300

    1670B.cpp

---
+++ codeforces/problems/1670B.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <array>
+#include <string>
+
+const int MAX_CHAR = 26;
+
+int main()
+{
+    int t(0);
+    std::cin >> t;
+    while(t--){
+        int n(0), k(0);
+        std::string s;
+        std::array<int, MAX_CHAR> special_alph;
+        std::cin >> n >> s >> k;
+
+        for (int i = 0; i < MAX_CHAR; i++){
+            special_alph[i] = 0;
+        }
+
+        for (int i = 0; i < k; i++){
+            char ch;
+            std::cin >> ch;
+            special_alph[ch - 'a'] = 1;
+        }
+
+        int count(0), answer(0);
+        for (int i = 0; i < n; i++){
+            if(special_alph[s[i] - 'a'] == 1){
+                answer = count > answer ? count : answer;
+                count = 1;
+            } else {
+                count++;
+            }
+        }
+        std::cout << answer << '\n';
+    }
+}
